d023e342e025dbc1bae0270d77a8977eb1985311
JunM1ao <45115605+JunM1ao@users.noreply.github.com>
2020-07-30 11:27:31 -1000

    Update UI.py

--- Components/UI.py
+++ Components/UI.py
@@ -72,123 +72,6 @@ def date_and_time_inputs(frame: LabelFrame, row: int, hour: int, exist: datetime
     return ret
 
 
-class Interface:
-    """
-    Interface class responsible for interactive GUI
-    """
-
-    def __init__(self):
-        """
-        Constructor for the Interface class
-        """
-        self.location = ""
-        self.bg_color = "#10ac84"
-        self.root = Tk()
-        self.root.title("Event Planner")
-        self.details = None
-        self.new = True
-        self.root.minsize(width=800, height=600)
-        self.background = Frame(self.root, bg=self.bg_color)
-        self.background.place(relwidth=1, relheight=1)
-
-        # build main window looks
-        menu_bar = Menu(self.root, tearoff=0)
-
-        def save_as():
-            return self.save_file(True)
-
-        def new_data():
-            global core
-            core = None
-            self.location = ""
-            self.reload_display()
-
-        file_sub_menu = Menu(menu_bar, tearoff=0)
-        file_sub_menu.add_command(label="New", command=new_data)
-        file_sub_menu.add_command(label="Open", command=self.get_file)
-        file_sub_menu.add_command(label="Save", command=self.save_file)
-        file_sub_menu.add_command(label="Save As...", command=save_as)
-        file_sub_menu.add_separator()
-        file_sub_menu.add_command(label="Exit", command=self.root.quit)
-
-        menu_bar.add_cascade(label="File", menu=file_sub_menu)
-
-        edit_sub_menu = Menu(menu_bar, tearoff=0)
-        edit_sub_menu.add_command(label="New Event", command=EventUI(self.root, self.reload_display).generate)
-
-        menu_bar.add_cascade(label="Edit", menu=edit_sub_menu)
-
-        self.root.config(menu=menu_bar)
-        self.root.mainloop()
-
-    def save_file(self, force: bool = False):
-        """
-        Method for saving the ICS file
-
-        Parameters
-        ----------
-        force: bool
-            whether or not to force ask for save location
-
-        Returns
-        -------
-        messagebox
-            if there is any error
-        """
-        global core
-        if not isinstance(core, CalendarCore):
-            return messagebox.showerror("No Data Error", "There is no event in the system to save")
-
-        if self.location == "" or force:
-            temp = filedialog.asksaveasfilename(initialdir="./", title="Select Save Location",
-                                                filetypes=(("Calendar File", "*.ics"), ("All Files", "*.*")))
-            if temp == "":
-                return
-            else:
-                self.location = temp
-
-            if not self.location.endswith(".ics"):
-                self.location += ".ics"
-        if self.location != "":
-            core.write_file(self.location)
-
-        messagebox.showinfo("Success!", f"Calendar data has been saved to:\n{self.location}")
-        self.reload_display()
-
-    def get_file(self):
-        """
-        Method to fetch the location of an existing ICS file
-
-        Returns
-        -------
-        messagebox
-            if there is any error
-        """
-        self.location = filedialog.askopenfilename(initialdir="./", title="Select Calendar File",
-                                                   filetypes=(("Calendar File", "*.ics"), ("All Files", "*.*")))
-        if self.location == "":
-            return
-        try:
-            global core
-            core = CalendarCore(self.location)
-        except TypeError:
-            return messagebox.showerror("Unknown File Type", "Only .ics files are supported")
-        self.new = False
-
-        self.reload_display()
-
-    def reload_display(self):
-        # reloading temporary data view of the CalendarCore, may be removed in the future
-        global core
-        temp = str(core)
-        if self.details:
-            self.details.destroy()
-
-        self.details = Label(self.background, text=temp, fg="white", bg="black", justify=LEFT)
-        self.details.grid(row=0, column=0) if core else self.details.destroy()
-        self.root.title(f"Event Planner - {self.location}") if self.location != "" else self.root.title("Event Planner")
-
-
 class EventUI:
     """
     UI class for create or edit event
@@ -515,3 +398,138 @@ class EventUI:
                         activebackground=self.bg).grid(column=i, row=0)
             i += 1
         change()
+
+
+class Interface:
+    """
+    Interface class responsible for interactive GUI
+    """
+
+    def __init__(self):
+        """
+        Constructor for the Interface class
+        """
+        self.location = ""
+        self.bg_color = "#10ac84"
+        self.root = Tk()
+        self.root.title("Event Planner")
+        self.details = None
+        self.new = True
+        self.root.minsize(width=800, height=600)
+        self.background = Frame(self.root, bg=self.bg_color)
+        self.background.place(relwidth=1, relheight=1)
+
+        # build main window looks
+        menu_bar = Menu(self.root, tearoff=0)
+
+        def save_as():
+            return self.save_file(True)
+
+        def new_data():
+            global core
+            core = None
+            self.location = ""
+            self.reload_display()
+
+        file_sub_menu = Menu(menu_bar, tearoff=0)
+        file_sub_menu.add_command(label="New", command=new_data)
+        file_sub_menu.add_command(label="Open", command=self.get_file)
+        file_sub_menu.add_command(label="Save", command=self.save_file)
+        file_sub_menu.add_command(label="Save As...", command=save_as)
+        file_sub_menu.add_separator()
+        file_sub_menu.add_command(label="Exit", command=self.root.quit)
+
+        menu_bar.add_cascade(label="File", menu=file_sub_menu)
+
+        edit_sub_menu = Menu(menu_bar, tearoff=0)
+        edit_sub_menu.add_command(label="New Event", command=EventUI(self.root, self.reload_display).generate)
+
+        menu_bar.add_cascade(label="Edit", menu=edit_sub_menu)
+
+        self.root.config(menu=menu_bar)
+        self.reload_display()
+        self.root.mainloop()
+
+    def save_file(self, force: bool = False):
+        """
+        Method for saving the ICS file
+
+        Parameters
+        ----------
+        force: bool
+            whether or not to force ask for save location
+
+        Returns
+        -------
+        messagebox
+            if there is any error
+        """
+        global core
+        if not isinstance(core, CalendarCore):
+            return messagebox.showerror("No Data Error", "There is no event in the system to save")
+
+        if self.location == "" or force:
+            temp = filedialog.asksaveasfilename(initialdir="./", title="Select Save Location",
+                                                filetypes=(("Calendar File", "*.ics"), ("All Files", "*.*")))
+            if temp == "":
+                return
+            else:
+                self.location = temp
+
+            if not self.location.endswith(".ics"):
+                self.location += ".ics"
+        if self.location != "":
+            core.write_file(self.location)
+
+        messagebox.showinfo("Success!", f"Calendar data has been saved to:\n{self.location}")
+        self.reload_display()
+
+    def get_file(self):
+        """
+        Method to fetch the location of an existing ICS file
+
+        Returns
+        -------
+        messagebox
+            if there is any error
+        """
+        self.location = filedialog.askopenfilename(initialdir="./", title="Select Calendar File",
+                                                   filetypes=(("Calendar File", "*.ics"), ("All Files", "*.*")))
+        if self.location == "":
+            return
+        try:
+            global core
+            core = CalendarCore(self.location)
+        except TypeError:
+            return messagebox.showerror("Unknown File Type", "Only .ics files are supported")
+        self.new = False
+
+        self.reload_display()
+
+    def reload_display(self):
+        # reloading temporary data view of the CalendarCore, may be removed in the future
+        global core
+
+        self.root.title(f"Event Planner - {self.location}") if self.location != "" else self.root.title("Event Planner")
+
+        if self.details:
+            self.details.destroy()
+
+        self.details = Frame(self.background, bg=self.bg_color)
+        self.details.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+        if not isinstance(core, CalendarCore):
+            top = Frame(self.details)
+            top.grid(row=0, column=0)
+            Label(self.details, font=("Comic Sans", 20), text="Select one of the following actions",
+                  bg=self.bg_color).grid(row=0, column=0)
+
+            bottom = Frame(self.details)
+            bottom.grid(row=1, column=0)
+            Button(bottom, text="Load ICS File", command=self.get_file, width=10, height=3).pack()
+            Button(bottom, text="New Event", width=10, height=3,
+                   command=EventUI(self.root, self.reload_display).generate).pack()
+        else:
+            core.data.sort()
+            temp = str(core)
+            Label(self.details, text=temp, fg="white", bg="black", justify=LEFT).grid(row=0, column=0)
